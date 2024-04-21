c7147ca56ead8438cf5a37a59d9397a344a075a0
xxhyz233 <49100459+xxhyz233@users.noreply.github.com>
2024-04-21 13:41:48 -0400

    Added fullscreen video script with animation

--- Final/script/script.js
+++ Final/script/script.js
@@ -1,20 +1,51 @@
-// Fullscreen image script
+// Fullscreen image and video script
 const columnItem = document.querySelectorAll(".column-item");
 const fullscreenView = document.querySelector(".fullscreen-view");
 const fullscreenImage = document.querySelector(".fullscreen-image");
+const fullscreenVideo = document.querySelector(".fullscreen-video");
 const closeBtn = document.querySelector(".close");
 
-if (!fullscreenView || !fullscreenImage || !closeBtn) {
+// Debugging fullscreen view
+if (!fullscreenView || (!fullscreenImage && !fullscreenVideo) || !closeBtn) {
   console.error("Necessary elements not found");
 }
 
 columnItem.forEach((item) => {
   item.addEventListener("click", () => {
-    const imgSrc = item.querySelector("img").src;
-    fullscreenImage.src = imgSrc;
-    fullscreenView.classList.add("show");
+    const hasVideo = item.querySelector("video") !== null;
+    const hasImg = item.querySelector("img") !== null;
+    // Don't Fullscreen .big-video
+    const hasBigVideo = item.querySelector(".big-video") !== null;
+
+    // Clear sources to avoid displaying old content
+    fullscreenImage.style.display = 'none';
+    fullscreenVideo.style.display = 'none';
+    
+    if (hasVideo) {
+
+      const vidSrc = item.querySelector("video").getElementsByTagName("source")[0].src;
+      fullscreenVideo.src = vidSrc;
+      fullscreenVideo.load();  // Ensure the video loads the new source
+      fullscreenVideo.muted = true;
+      fullscreenVideo.play();
+      fullscreenVideo.style.display = 'block'; // Ensure video is visible
+      fullscreenImage.src = ''; // Clear image source
+    }
+    else if (hasImg) {
+      const imgSrc = item.querySelector("img").src;
+      fullscreenImage.src = imgSrc;
+      fullscreenImage.style.display = 'block'; // Ensure image is visible
+      fullscreenVideo.src = ''; // Clear video source
+    }
+    if (hasBigVideo) {
+      fullscreenView.classList.remove("show");
+    }
+    else {
+      fullscreenView.classList.add("show");
+    }
+    
     gsap.fromTo(
-      fullscreenImage,
+      hasVideo ? fullscreenVideo : fullscreenImage,
       { opacity: 0, ease: "none" },
       { opacity: 1, duration: 0.2 }
     );
@@ -28,7 +59,6 @@ if (closeBtn) {
 }
 
 fullscreenView.addEventListener("click", (event) => {
-  console.log("Clicked element:", event.target); // Debugging line
   if (event.target === fullscreenView || event.target === closeBtn) {
     closeFullscreen();
   }
@@ -39,31 +69,39 @@ fullscreenImage.addEventListener("click", (event) => {
 });
 
 function closeFullscreen() {
+  // Clears the source for img and vid
+  fullscreenImage.src = '';
+  fullscreenVideo.src = '';
   fullscreenView.classList.remove("show");
 }
 
 
 // DOM load before GSAP animation 
 document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault;
   // Header scroll animation
   let lastScrollTop = 0;
-  const header = document.querySelector('.sticky'); // Select your header class
+  const header = document.querySelector('.sticky');
+  // Make the scrollable elements in the header into an array to create stagger
+  const scrollElements = gsap.utils.toArray(".scAni");
   window.addEventListener('scroll', function() {
       let scrollTop = window.scrollY || document.documentElement.scrollTop;
       if (scrollTop > lastScrollTop) {
           // Scroll down animation
           gsap.to(header, { y: '-100%', duration: 0.3 });
+          gsap.to(scrollElements, { y: '-500%', duration: 0.2, stagger: 0.04});
       } else {
           // Scroll up animation
           gsap.to(header, { y: '0%', duration: 0.3 });
+          gsap.to(scrollElements, { y: '0%', duration: 0.2, stagger: 0.04});
       }
       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
   }, false);
 
   // Page load animation
-  const ani = gsap.utils.toArray(".ani");
+  const loadAni = gsap.utils.toArray(".loadAni");
   gsap.fromTo(
-    ani,
+    loadAni,
     {
       opacity: 0,
       ease: "power2.out",
@@ -77,9 +115,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
   );
   
   // For slower pages like FILM/INFO
-  const slowAni = gsap.utils.toArray(".slowAni");
+  const slowLoadAni = gsap.utils.toArray(".slowLoadAni");
   gsap.fromTo(
-    slowAni,
+    slowLoadAni,
     {
       opacity: 0,
       ease: "power2.out",
